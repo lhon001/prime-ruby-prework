ca4ba582aa8de69aa048dbfb37c9d0db2a8cac05
Lawrence Hon <lhon001@gmail.com>
2018-04-02 04:40:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(num)
+<<<<<<< HEAD
   if num <= 1
+=======
+  if num <= 0
+>>>>>>> 33cd85546d5e0ed09a49cbd297c6c6282e899db9
     return false
   end
 
@@ -10,3 +14,7 @@ def prime?(num)
   end
   true
 end
+<<<<<<< HEAD
+=======
+
+>>>>>>> 33cd85546d5e0ed09a49cbd297c6c6282e899db9
